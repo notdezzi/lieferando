bc0bf8fb03734b7ad98461fa0b48d3fb77a17d09
max <45425238+notdezzi@users.noreply.github.com>
2024-11-20 09:33:28 +0100

    layout.tsx updated, navbar added -> finn

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,6 @@ import "./globals.css";
 import { NextAuthProvider } from "../providers/NextAuthProvider";
 import drinkanddrivesmallw from '../public/drink_and_drive_small_white.png';
 import Image from "next/image";
-import styles from './layout.module.css';
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -31,6 +30,20 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <NextAuthProvider>
+          <nav>
+            <Image
+            alt="Logo"
+            src={drinkanddrivesmallw}
+            style={{
+              maxWidth: '200px',
+              height: 'auto',
+            }}
+            />
+            <div className="wrapper-search">
+              <input type="text" className="searchbar" placeholder="Suche hier nach deiner Adresse..."></input>
+              <button className="searchbutton"></button>
+            </div>
+          </nav>
           {children}
           </NextAuthProvider>
       </body>
