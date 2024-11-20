1a82c6dada1b35fa3f6faeb05510fb4c85ab1a76
finnj537 <finn@zeitreisende.eu>
2024-11-20 11:07:18 +0100

    landingpage

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,9 +42,9 @@ export default function RootLayout({
             />
             <div className="wrapper-search">
               <input type="text" className="searchbar" placeholder="Suche hier nach deiner Adresse..."></input>
-              <button className="searchbutton"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-search"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg></button>
+              <button className="searchbutton"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-search"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg></button>
             </div>
-            <button className="profile"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg></button>
+            <button className="profile"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-user"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg></button>
           </nav>{children}</NextAuthProvider>
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,8 @@ import {
 } from "@/components/buttons.component";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
+import Image from "next/image";
+import drinkanddrive from '../public/drinkanddrive.png';
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
@@ -17,18 +19,30 @@ export default async function Home() {
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
-        height: "70vh",
+        width: "100vw",
+        height: "80vh",
       }}
     >
-      <div>
-        <LoginButton />
-        <RegisterButton />
-        <LogoutButton />
-        <ProfileButton />
+      <div className="wrapper-content">
+        <Image
+        alt="Logo"
+        src={drinkanddrive}
+        style= {{
+          marginBottom: "1rem",
+        }}
 
-        <h1>Server Session</h1>
-        <pre>{JSON.stringify(session)}</pre>
+        />
+        <div className="wrapper-search-pg" 
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          <input type="text" className="searchbar" placeholder="Suche hier nach deiner Adresse..."></input>
+          <button className="searchbutton"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-search"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg></button>
+        </div>
       </div>
+
     </main>
   );
 }
