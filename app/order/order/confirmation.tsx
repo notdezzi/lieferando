1b510121e626658e04ef5b49bed930795ef4f3ab
max <45425238+notdezzi@users.noreply.github.com>
2024-11-25 15:21:25 +0100

    Create confirmation.tsx

---
+++ app/order/order/confirmation.tsx
@@ -0,0 +1,13 @@
+import { PrismaClient } from '@prisma/client';
+import Link from 'next/link';
+
+const prisma = new PrismaClient();
+
+export default async function OrderConfirmedPage() {
+  return (
+    <div>
+
+        ORder confirmd
+    </div>
+  );
+}
