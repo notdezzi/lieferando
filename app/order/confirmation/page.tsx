8c29c4de2e86edeee0d1ef861cd97198fbb3a41c
Zirikus <Zirikus13@gmail.com>
2024-11-27 16:03:25 +0100

    confirmation page

---
+++ app/order/confirmation/page.tsx
@@ -0,0 +1,8 @@
+export default function OrderPaymentPage() {
+    return(<div className="max-w-2xl mx-auto p-4 space-y-6">
+    <div className="space-y-6">
+      <h2 className="text-xl font-semibold">Ordrrrr confrmd</h2>
+    </div>
+    </div>)
+    
+}
