2e82f427848473a62eff60ea746ab422165d8887
Zirikus <Zirikus13@gmail.com>
2024-11-27 13:33:20 +0100

    anfang payment side

--- app/order/confirmation/page.tsx
+++ app/order/confirmation/page.tsx
@@ -5,8 +5,28 @@ const prisma = new PrismaClient();
 
 export default async function OrderConfirmedPage() {
   return (
-    <div>
-        ORder confirmd
+    <div className="max-w-2xl mx-auto p-4 space-y-6">
+      <div className="space-y-6">
+        {/* Payment Options */}
+        <h2 className="text-xl font-semibold">Payment Options</h2>
+        <div className="space-y-2">
+        <button
+          className="w-full"
+          //onClick={}
+        >Pay with PayPal
+        </button>
+        <button
+          className="w-full"
+          //onClick={}
+        >Pay with Credit Card
+        </button>
+        <button
+          className="w-full"
+          //onClick={}
+        >Back to Order
+        </button>
+        </div>
+      </div>
     </div>
   );
 }
